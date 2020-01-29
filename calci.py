8455e819a470c0863419a637d69ee924d9806db2
girija25 <60418897+girija25@users.noreply.github.com>
2020-01-29 10:53:02 +0530

    Create calci.py

---
+++ calci.py
@@ -0,0 +1,17 @@
+print ("CALCULATOR")
+def add(a,b):
+    c=a+b
+    print (c)
+def sub(a,b):
+    c=a-b
+    print (c)
+def mul(a=22,b=25):
+    c=a*b
+    print (c)
+def div(a,b):
+    c=a/b
+    print (c)
+add(22,25)
+sub(a=22,b=25)
+mul()
+div(11,25)
